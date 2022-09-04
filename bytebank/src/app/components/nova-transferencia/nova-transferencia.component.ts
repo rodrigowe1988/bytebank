4c062491f653a14427e776078748cf31501e75e8
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-09-04 18:20:11 -0300

    forms nova_transferencia

---
+++ bytebank/src/app/components/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  valor: number = 0;
+  destino: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
