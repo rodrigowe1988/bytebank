fab081ec6d042b5ead58b6063306a5f890166064
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-09-05 08:00:00 -0300

    criando mensagem com ngIf / criando primeiro service

--- bytebank/src/app/components/extrato/extrato.component.ts
+++ bytebank/src/app/components/extrato/extrato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TransferenciaService } from 'src/app/services/transferencia.service';
 
 @Component({
   selector: 'app-extrato',
@@ -6,9 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
-  @Input() transferencias: any[] = [];
+  transferencias: any[] = [];
 
-  constructor() {}
+  constructor(private service: TransferenciaService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
+  }
 }
