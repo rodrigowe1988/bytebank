348d365ed33c71c6b41ffc61c931026beb05cdd5
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-09-04 21:58:21 -0300

    transferencias input/output

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencia: any;
+  transferencias: any[] = [];
 
   transferir($event: any): void {
-    console.log($event)
-    this.transferencia = $event;
+    console.log($event);
+    const transferencia = { ...$event, data: new Date() };
+    this.transferencias.push(transferencia);
   }
-  
 }
