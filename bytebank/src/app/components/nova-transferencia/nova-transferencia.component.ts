dcb259ae0c67a641724786da73d1f7d571011092
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-09-04 21:08:18 -0300

    exportando dados do componente

--- bytebank/src/app/components/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/components/nova-transferencia/nova-transferencia.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number = 0;
   destino: number = 0;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  transferir(): void {
+    console.log('Solicitada nova transferência...');
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
+    this.limparCampos();
   }
 
+  limparCampos() {
+    this.valor = Number('');
+    this.destino = Number('');
+  }
 }
